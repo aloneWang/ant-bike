36d09759e0d192415a7b7c75d2221030a1c4acd4
wangjianping <alone0228@126.com>
2019-06-01 17:59:45 +0800

    无效提交

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -6,6 +6,7 @@ import './login.scss'
 class Login extends Component {
   
   render() {
+    const { getFieldDecorator } = this.props.form
     return(
       <div className='login-page'>
             <header className='login-header'>
@@ -25,61 +26,25 @@ class Login extends Component {
                   <div className='title'>
                     欢迎你，请先登录
                   </div>
-                  {/*<Card>
-                    <Form style={{ width: 260 }}>
-                      <FormItem>
-                        {
-                          getFieldDecorator('userName', {
-                            initialValue: '',
-                            rules: [
+                  <Card>
+                    <Form style={{width:260}}>
+                      <Form.Item>
+                        {getFieldDecorator('userName', {
+                            initiaValue:'',
+                            rules:[
                               {
                                 required: true,
-                                message: '用户名不为空'
-                              },
-                              {
-                                min: 5,
-                                max: 10,
-                                message: '用户名长度不在范围内'
-                              },
-                              {
-                                pattern: new RegExp('^\\w+$', 'g'),
-                                message: '用户名必须为字母或者数字'
+                                message:'用户名不为空'
+                              },{
+                                pattern: new RegExp('^\\w+$','g'),
+                                message:'用户名必须为字母或者数字'
                               }
                             ]
                           })(<Input prefix={<Icon type='user' />} placeholder='请输入用户名' />)
                         }
-
-                      </FormItem>
-                      <FormItem>
-                        {
-                          getFieldDecorator('userPwd', {
-                            initialValue: '',
-                            rules: [
-                              {
-                                required: true,
-                                message: '密码不为空'
-                              }
-                            ]
-                          })(<Input type='password' prefix={<Icon type='lock' />} placeholder='请输入密码' />)
-                        }
-                      </FormItem>
-                      <FormItem>
-                        {
-                          getFieldDecorator('remember', {
-                            valuePropName: 'checked',
-                            initialValue: false,
-                            rules: []
-                          })(
-                            <Checkbox>记住密码</Checkbox>
-                          )
-                        }
-                        <a href="#/login" style={{ float: 'right' }}>忘记密码</a>
-                      </FormItem>
-                      <FormItem>
-                        <Button type='primary' block onClick={this.handleSubmit}>登录</Button>
-                      </FormItem>
+                      </Form.Item>
                     </Form>
-                      </Card>*/}
+                  </Card>
                 </div>
               </div>
             </div>
@@ -88,4 +53,4 @@ class Login extends Component {
   }
 }
 
-export default Login
+export default Form.create()(Login)
